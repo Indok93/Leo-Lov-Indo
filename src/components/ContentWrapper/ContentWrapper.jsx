20d0acfc21ef03fac3fdba043d90ebe2e9bbf4ac
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-01-12 07:47:34 +0100

    updated

--- src/components/ContentWrapper/ContentWrapper.jsx
+++ src/components/ContentWrapper/ContentWrapper.jsx
@@ -1,8 +1,10 @@
-
+import image1 from "../../assets/images/2.jpg"
+import styles from "./ContentWrapper.module.scss"
 
 export const ContentWrapper = ({ title, children }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
+        <img src={image1} alt="image1" />
         <h2>{title}</h2>
         <>{children}</>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,9 @@ export const Home = () => {
   return (
     <main>
       <CardWrapper />
-      <ContentWrapper title="Forside">
-        <p>Velkommen til min side</p>
+      <ContentWrapper title="INTET PROBLEM">
+        <p>Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen.</p>
+        <p>Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!</p>
       </ContentWrapper>
     </main>
   )
